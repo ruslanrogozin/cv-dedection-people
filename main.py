ce12377dbad432ee576fdd39af32866e66e9a86f
ruslan_rogozin <ruslan-96-01-09@mail.ru>
2023-07-17 19:23:22 +0300

    check

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import sys
 from pathlib import Path
-from PIL import Image
-from tqdm import tqdm
+
 import numpy as np
 import torch
-from torchvision.models.detection import fasterrcnn_resnet50_fpn
-
-from scripts.dataloader import ImagesDataset
+from PIL import Image
 from scripts.convert_and_save import convert_and_save
+from scripts.dataloader import ImagesDataset
+from torchvision.models.detection import fasterrcnn_resnet50_fpn
 from torchvision.transforms.functional import pil_to_tensor
+from tqdm import tqdm
 
-def main(): 
+
+def main():
     IMAGE_DIR = Path('data')
     jpg = list(IMAGE_DIR.rglob('*.jpg'))
     jpeg = list(IMAGE_DIR.rglob('*.jpeg'))
@@ -23,25 +24,22 @@ def main():
 
     images.sort()
 
-
     if not images:
         sys.exit('Data directory is empty')
 
     data = ImagesDataset(images)
-#print(data.files)
+    # print(data.files)
 
     model = fasterrcnn_resnet50_fpn(weights=None, progress=False)
 
     state_dict = torch.load('model\model.DEFAULT')
     model.load_state_dict(state_dict)
-    model.eval(); ## Setting Model for Evaluation/Prediction
+    model.eval();  ## Setting Model for Evaluation/Prediction
 
     if not (Path.cwd() / 'new_data').exists():
-
         Path("new_data").mkdir(parents=True, exist_ok=True)
 
-
-    for i, image in  tqdm(enumerate(data), ncols=80):
+    for i, image in tqdm(enumerate(data), ncols=80):
 
         name, format = data.files[i].name.rsplit('.', 1)
         scale = image[1]
@@ -55,8 +53,7 @@ def main():
             convert_and_save(picture_out, original_image, name, format, scale)
         else:
             continue
-    
-    
-    
+
+
 if __name__ == "__main__":
     main()
--- scripts/dataloader.py
+++ scripts/dataloader.py
@@ -3,22 +3,22 @@ from torchvision import transforms
 from torchvision.datasets.folder import pil_loader
 
 
-
 class ImagesDataset(Dataset):
     """
     Датасет с картинками, который паралельно подгружает их из папок
     производит скалирование и превращение в торчевые тензоры
     """
-    def __init__(self, files,  transform = None, rescale_size = 600):
+
+    def __init__(self, files, transform=None, rescale_size=600):
         super().__init__()
         self.files = sorted(files)
         self.rescale_size = rescale_size
         self._len = len(self.files)
         self.transform = transform
         if self.transform is None:
-            self.transform  = transforms.Compose([
-                  transforms.Resize(size=(self.rescale_size, self.rescale_size)),
-                  transforms.ToTensor()           ])
+            self.transform = transforms.Compose([
+                transforms.Resize(size=(self.rescale_size, self.rescale_size)),
+                transforms.ToTensor()])
 
     def __len__(self):
         return self._len
@@ -27,10 +27,9 @@ class ImagesDataset(Dataset):
         ''' return torch.tensor(image) and shape original image'''
         file = self.files[index]
 
-
         x = pil_loader(file)
         width, height = x.size
         x_scale = self.rescale_size / width
         y_scale = self.rescale_size / height
         x = self.transform(x)
-        return x.unsqueeze(dim=0) , (x_scale, y_scale)
+        return x.unsqueeze(dim=0), (x_scale, y_scale)
