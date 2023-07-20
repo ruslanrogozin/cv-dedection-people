9d22a78a7dc5137ee5b1000c2983695a167966a4
ruslan_rogozin <ruslan-96-01-09@mail.ru>
2023-07-20 18:43:21 +0300

    save

--- ssd/entrypoints.py
+++ ssd/entrypoints.py
@@ -55,6 +55,9 @@ def nvidia_ssd(pretrained=True, **kwargs):
         model_math (str, 'fp32'): returns a model in given precision ('fp32' or 'fp16')
         
     """
+    from . import model as ssd
+    model = ssd.SSD300()
+    
     if pretrained:
         #'https://api.ngc.nvidia.com/v2/models/nvidia/ssd_pyt_ckpt_amp/versions/20.06.0/files/nvidia_ssdpyt_amp_200703.pt'
         checkpoint = 'https://api.ngc.nvidia.com/v2/models/nvidia/ssd_pyt_ckpt_amp/versions/20.06.0/files/nvidia_ssdpyt_amp_200703.pt'
@@ -65,11 +68,13 @@ def nvidia_ssd(pretrained=True, **kwargs):
         else:
             ckpt_file  = os.path.join('ssd', os.path.basename(checkpoint))
             
-        print(ckpt_file )
+        
         if not torch.cuda.is_available():
             ckpt = torch.load(ckpt_file, map_location=torch.device('cpu'))
-        #ckpt = ckpt['model']
+        ckpt = ckpt['model']
   
-        #if checkpoint_from_distributed(ckpt):
-            #ckpt = unwrap_distributed(ckpt)
-        #m.load_state_dict(ckpt)
+        if checkpoint_from_distributed(ckpt):
+            ckpt = unwrap_distributed(ckpt)
+        model.load_state_dict(ckpt)
+        
+    return model
