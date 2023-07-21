d3aa4f35936de2a073206994fdf802aa101fe9d0
ruslan_rogozin <ruslan-96-01-09@mail.ru>
2023-07-21 11:49:15 +0300

    run batch

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import os
 
 from ssd.model import ResNet
 from ssd.entrypoints import _download_checkpoint, nvidia_ssd
+from ssd.nvidia_ssd_processing_utils import Processing as processing
+    
 
 def main():
     return 0
@@ -20,6 +22,17 @@ if __name__ == "__main__":
     IMAGE_SIZE = (300, 300)
     backbone = ResNet()
     model = nvidia_ssd() 
+    model.eval()
+    uris = [
+    'http://images.cocodataset.org/val2017/000000397133.jpg',
+    'http://images.cocodataset.org/val2017/000000037777.jpg',
+    'http://images.cocodataset.org/val2017/000000252219.jpg'
+     ]
+    inputs = [processing.prepare_input(uri) for uri in uris]
+    #precision = 'fp32'
+    tensor = processing.prepare_tensor(inputs)
+    with torch.no_grad():
+        detections_batch = model(tensor)
     
     
     #main()
