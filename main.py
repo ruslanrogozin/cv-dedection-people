1b6d6f74fd29435025e7d131eec94a3eac9d5268
ruslan_rogozin <ruslan-96-01-09@mail.ru>
2023-07-21 12:54:44 +0300

    model load from url and predict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from pathlib import Path
-
+import cv2
 import torch
 import torch.nn as nn
 from torchsummary import summary
@@ -10,7 +10,7 @@ import os
 from ssd.model import ResNet
 from ssd.entrypoints import _download_checkpoint, nvidia_ssd
 from ssd.nvidia_ssd_processing_utils import Processing as processing
-    
+
 
 def main():
     return 0
@@ -33,6 +33,34 @@ if __name__ == "__main__":
     tensor = processing.prepare_tensor(inputs)
     with torch.no_grad():
         detections_batch = model(tensor)
+    results_per_input = processing.decode_results(detections_batch)
+    best_results_per_input = [processing.pick_best(results, 0.40) for results in results_per_input]
+    
+    bboxes, classes, confidences = best_results_per_input[2]
+    
+    image = inputs[2].copy()
+    orig_h, orig_w = image.shape[0], image.shape[1]
+    print(classes, bboxes)
+    for idx in range(len(bboxes)):
+        if classes[idx] == 1:
+            # get the bounding box coordinates in xyxy format
+            x1, y1, x2, y2 = bboxes[idx]
+            # resize the bounding boxes from the normalized to 300 pixels
+            x1, y1 = int(x1*300), int(y1*300)
+            x2, y2 = int(x2*300), int(y2*300)
+            # resizing again to match the original dimensions of the image
+            x1, y1 = int((x1/300)*orig_w), int((y1/300)*orig_h)
+            x2, y2 = int((x2/300)*orig_w), int((y2/300)*orig_h)
+            # draw the bounding boxes around the objects
+            cv2.rectangle(
+                image, (x1, y1), (x2, y2), (0, 0, 255), 2, cv2.LINE_AA
+            )
+    
+    
+    
+    cv2.imshow('image', image)
+    cv2.imwrite()
+    cv2.waitKey(0)
     
     
     #main()
---
+++ ssd/convert_and_save.py
@@ -0,0 +1,34 @@
+import torch
+from torchvision.utils import draw_bounding_boxes
+import torchvision.transforms.functional as F
+
+
+def convert_and_save(prediction, original_image, name, format_data, scale, threshold=0.8, path='new_data/'):
+    ''' function for convetr and save pictures'''
+
+    prediction = prediction.copy()
+    prediction[0]["boxes"] = prediction[0]["boxes"][prediction[0]["scores"] > threshold]
+    prediction[0]["labels"] = prediction[0]["labels"][prediction[0]["scores"] > threshold]
+    prediction[0]["scores"] = prediction[0]["scores"][prediction[0]["scores"] > threshold]
+
+    prediction[0]["boxes"] = prediction[0]["boxes"][prediction[0]["labels"] == 1]
+
+    new_boxes = prediction[0]["boxes"]
+
+    new_b_b = []
+    for i in new_boxes:
+        bbx = i.detach().numpy()
+        x1, y1, x2, y2 = bbx[0] / scale[0], bbx[1] / scale[1], bbx[2] / scale[0], bbx[3] / scale[1]
+        new_b_b.append([x1, y1, x2, y2])
+
+    new_b_b = torch.tensor(new_b_b)
+
+    image_output = draw_bounding_boxes(image=original_image,
+                                       boxes=new_b_b,
+                                       # @labels=holiday_annot_labels,
+                                       colors="red",
+                                       width=2)
+
+    img = image_output.detach()
+    img = F.to_pil_image(img)
+    img.save(path + 'new_' + name + '.' + format_data)
