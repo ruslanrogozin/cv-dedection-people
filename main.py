545ccb78859458a09c051b86bd3e959ec1e29fdb
ruslan_rogozin <ruslan-96-01-09@mail.ru>
2023-08-03 18:10:25 +0300

    2

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from pathlib import Path
 from ssd.create_model import nvidia_ssd
 from config.config import Configs
 from detect_images import detect_images
 from detect_video import detect_video
+
+from pathlib import Path
 
 
 def main():
@@ -14,23 +15,19 @@ def main():
         pretrained_default=True,
         pretrainded_custom=False,
         path=work_directory / configs.path_weight_model,
-        device=device
+        device=device,
     )
     model.eval()
 
     try:
-        detect_images(model=model,
-                      configs=configs,
-                      work_directory=work_directory)
+        detect_images(model=model, configs=configs, work_directory=work_directory)
     except Exception:
-        print('no images found!')
+        print("no images found!")
 
     try:
-        detect_video(model=model,
-                     configs=configs,
-                     work_directory=work_directory)
+        detect_video(model=model, configs=configs, work_directory=work_directory)
     except Exception:
-        print('no video found!')
+        print("no video found!")
 
 
 if __name__ == "__main__":
