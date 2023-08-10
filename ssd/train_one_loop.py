d7cd1a45e08c9213d88badb55558b7f4d08543c9
ruslan_rogozin <ruslan-96-01-09@mail.ru>
2023-08-10 23:23:10 +0300

    add function for train loop

---
+++ ssd/train_one_loop.py
@@ -0,0 +1,28 @@
+
+from torch.autograd import Variable
+from tqdm.auto import tqdm
+
+
+def train_one_loop(model, optimizer, loss_func, train_dataloader, device):
+    print("Training")
+    loss_sum = 0
+    for data in tqdm(train_dataloader, total=len(train_dataloader)):
+        # for nbatch, data in enumerate(train_dataloader):
+
+        optimizer.zero_grad()
+
+        img, _, _, bbox_data, bbox_labels = data
+
+        gloc = Variable(bbox_data.transpose(1, 2).contiguous(), requires_grad=False).to(
+            device
+        )
+        glabel = Variable(bbox_labels, requires_grad=False).to(device)
+        #
+        ploc, plabel = model(img.to(device))
+        # print(ploc.device, plabel.device, gloc.device, glabel.device)
+        loss = loss_func(ploc=ploc, plabel=plabel, gloc=gloc, glabel=glabel)
+        loss_sum += loss.item()
+        loss.backward()
+        optimizer.step()
+
+    return loss_sum / len(train_dataloader)
