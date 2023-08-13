46d88fcfad8aa606a38a541588d73286093e9de9
ruslan_rogozin <ruslan-96-01-09@mail.ru>
2023-08-13 14:56:12 +0300

    add pick best in model eval and plot result class in noteboobs

--- config/config.py
+++ config/config.py
@@ -8,3 +8,4 @@ class Configs:
     batch_size = 64
     use_padding_in_image_transform = False#True
     decode_result = {"criteria": 0.5, "max_output": 200, "pic_threshold": 0.3}
+    use_pick_best_in_eval = True
--- ssd/model_eval.py
+++ ssd/model_eval.py
@@ -2,8 +2,16 @@ import torch
 from torchmetrics.detection.mean_ap import MeanAveragePrecision
 from tqdm.auto import tqdm
 
+from config.config import Configs
 
-def model_evaluate(model, encoder, val_dataloader, device):
+
+def model_evaluate(
+    model,
+    encoder,
+    val_dataloader,
+    device=Configs.device,
+    use_pick_best=Configs.use_pick_best_in_eval,
+):
     target = []
     preds = []
     print("Evaluating")
@@ -12,15 +20,36 @@ def model_evaluate(model, encoder, val_dataloader, device):
         img, _, images_sizes, bbox_data, bbox_labels = data
         with torch.no_grad():
             ploc, plabel = model(img.to(device))
-            detections = encoder.decode_batch(ploc, plabel, 0.5, 200)
+            detections = encoder.decode_batch(
+                ploc,
+                plabel,
+                Configs.decode_result["criteria"],
+                Configs.decode_result["max_output"],
+            )
 
         for idx in range(ploc.shape[0]):
             true_dict = dict()
             preds_dict = dict()
             htot, wtot = images_sizes[0][idx].item(), images_sizes[1][idx].item()
-            pred_bbx = detections[idx][0]
+
+            if use_pick_best:
+                best_score = (
+                    (detections[idx][2] > Configs.decode_result["pic_threshold"])
+                    .detach()
+                    .cpu()
+                )
+
+                pred_bbx = detections[idx][0][best_score, :]
+                labels_pred = detections[idx][1][best_score]
+                scores_pred = detections[idx][2][best_score]
+            else:
+                pred_bbx = detections[idx][0]
+                labels_pred = detections[idx][1]
+                scores_pred = detections[idx][2]
+
             tr_bbx = bbox_labels[idx] > 0
             bbx_target = []
+
             for j in range(len(bbox_data[idx][tr_bbx])):
                 l, t, r, b = bbox_data[idx][tr_bbx][j].detach().cpu().numpy()
                 bbx_target.append([l * wtot, t * htot, r * wtot, b * htot])
@@ -32,13 +61,12 @@ def model_evaluate(model, encoder, val_dataloader, device):
 
             true_dict["boxes"] = torch.tensor(bbx_target).detach().cpu()
             true_dict["labels"] = bbox_labels[idx][tr_bbx].detach().cpu()
+
             preds_dict["boxes"] = torch.tensor(bbx_pred).detach().cpu()
-            preds_dict["scores"] = detections[idx][2].detach().cpu()
-            preds_dict["labels"] = detections[idx][1].detach().cpu()
+            preds_dict["scores"] = scores_pred.detach().cpu()
+            preds_dict["labels"] = labels_pred.detach().cpu()
             preds.append(preds_dict)
             target.append(true_dict)
-        #if i == 10:
-            #break
 
     metric = MeanAveragePrecision()
     metric.update(preds, target)
