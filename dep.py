f9069c410e75c6d1dd2d46a998a3735f38bc5e5c
ruslan_rogozin <ruslan-96-01-09@mail.ru>
2023-08-27 22:59:18 +0300

    deploy

--- dep.py
+++ dep.py
@@ -1,100 +1,11 @@
 from pathlib import Path
 from typing import Annotated
 
-import cv2
-import torch
 from fastapi import FastAPI, HTTPException
 
 from config.config import Configs
+from Detection import Detection
 from ssd.create_model import nvidia_ssd
-from ssd.dataloader import ImagesDataset
-from ssd.decode_results import Processing as processing
-
-
-class Detection:
-    def __init__(
-        self,
-    ):
-        self.work_directory = Path().cwd()
-        self.device = Configs.device
-        self.label_num = 81
-        self.pretrained_default = True
-        self.pretrainded_custom = False
-        model = nvidia_ssd(
-            pretrained_default=self.pretrained_default,
-            pretrainded_custom=self.pretrainded_custom,
-            path=self.work_directory / "weight",
-            device=self.device,
-            label_num=self.label_num,
-        )
-        self.info = {1: "person", 2: "head"}
-        self.model = model
-        self.weight = "default"
-        self.use_head = False
-
-    def detect_image_from_folder(
-        self,
-        path_to_data=Configs.path_data,
-        criteria_iou=Configs.decode_result["criteria"],
-        max_output_iou=Configs.decode_result["max_output"],
-        prob_threshold=Configs.decode_result["pic_threshold"],
-    ):
-        path_to_data = Path(path_to_data)
-        data = ImagesDataset(
-            path=path_to_data,
-            device=self.device,
-        )
-
-        if len(data.images) == 0:
-            return "no images found!"
-
-        self.model.eval()
-        ans = {}
-        for image, file in data:
-            ans.setdefault(file, {})
-            image = image.unsqueeze(0)
-            if self.device == "cuda" and torch.cuda.is_available():
-                image = image.cuda()
-
-            with torch.no_grad():
-                detections = self.model(image)
-
-            results_per_input = processing.decode_results(
-                predictions=detections,
-                criteria=criteria_iou,
-                max_output=max_output_iou,
-            )
-
-            best_results_per_input = processing.pick_best(
-                detections=results_per_input[0],
-                threshold=prob_threshold,
-            )
-
-            original = cv2.imread(str(file))
-
-            orig_h, orig_w = original.shape[0], original.shape[1]
-            bboxes, classes, _ = best_results_per_input
-
-            ans[file]["person"] = []
-            if self.use_head:
-                ans[file]["head"] = []
-
-            for idx, bbox in enumerate(bboxes):
-                if not self.use_head and (classes[idx] != 1):
-                    continue
-                x1, y1, x2, y2 = bbox
-                # resize the bounding boxes from the normalized to 300 pixels
-                x1, y1 = int(x1 * 300), int(y1 * 300)
-                x2, y2 = int(x2 * 300), int(y2 * 300)
-                # resizing again to match the original dimensions of the image
-                x1, y1 = int((x1 / 300) * orig_w), int((y1 / 300) * orig_h)
-                x2, y2 = int((x2 / 300) * orig_w), int((y2 / 300) * orig_h)
-                # draw the bounding boxes around the objects
-                info = self.info[classes[idx]]
-                ans[file][info].append([x1, y1, x2, y2])
-
-        return ans
-
 
 detection = Detection()
 
